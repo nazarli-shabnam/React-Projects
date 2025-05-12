ff609aef72598bb29f0ab08def632e244a247297
nazarli-shabnam <shabnamnezerli@gmail.com>
2025-05-12 18:52:18 +0400

    some css to FoodItem.jsx

--- recipe-app/src/components/FoodItem.jsx
+++ recipe-app/src/components/FoodItem.jsx
@@ -1,9 +1,13 @@
+import styles from './fooditem.module.css';
 export default function FoodItem({ food }) {
     return (
-      <div>
-        <img src={food.image} alt={food.title} />
-        <h1>{food.title}</h1>
-        <button>View Recipe</button>
+      <div className={styles.itemContainer}>
+        <img className={styles.itemImage} src={food.image} alt={food.title} />
+        <div className={styles.itemContent}>
+          <p className={styles.itemName}>{food.title}</p>
+          </div>
+         <div className={styles.buttonContainer}>        <button className={styles.itemButton}>View Recipe</button>
+</div>
       </div>
     );
   }
