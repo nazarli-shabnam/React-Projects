4e26b3ce4ee35679d6bfc0e94b5c12c0057d87d4
nazarli-shabnam <shabnamnezerli@gmail.com>
2025-05-25 15:29:16 +0400

    ingredients

--- recipe-app/src/components/FoodDetails.jsx
+++ recipe-app/src/components/FoodDetails.jsx
@@ -1,20 +1,61 @@
 import { useEffect } from "react";
 import { useState } from "react";
-export default function FoodDetails({foodId}){
-    const [food,setFood]= useState({})
-    const URL= `https://api.spoonacular.com/recipes/${foodId}/information`
-    const API_KEY="e9f14b6dedc54e7d9d1d92380399152b"
-useEffect(()=>{
-    async function fetchFood(){
-       const res= await fetch(`${URL}?apiKey=${API_KEY}`)
-      const data= await res.json()
-      console.log(data)
-      setFood(data)
+export default function FoodDetails({ foodId }) {
+    const [isLoading, setIsLoading] = useState(true);
+  const [food, setFood] = useState({});
+  const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
+  const API_KEY = "e9f14b6dedc54e7d9d1d92380399152b";
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data);
+      setFood(data);
+      setIsLoading(false);
     }
-    fetchFood()
-},[foodId])
-    return <div>Food Detail {foodId}
-    {food.title}
-    <img src={food.image} alt={food.title} />
+    fetchFood();
+  }, [foodId]);
+   return (
+    <div>
+      <div>
+        <h1>{food.title}</h1>
+
+        <img src={food.image} alt={food.title} />
+
+        <div>
+          <span>
+            <strong>⏰ {food.readyInMinutes} Minutes</strong>
+          </span>
+          <span>
+            {food.servings} <strong>👪 Servings</strong>
+          </span>
+          <span>
+            {food.vegetarian ? "🍲 Vegetarian" : "🍖 Not Vegetarian"}
+          </span>
+          <span>
+            {food.vegan ? "🌱 Vegan" : "🍖 Not Vegan"}
+          </span>
+        </div>
+
+        <div>
+          💲<span>{(food.pricePerServing / 100).toFixed(2)} Per serving</span>
+        </div>
+      </div>
+
+      <div>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <div>
+            <h2>Ingredients</h2>
+            <ul>
+              {food.extendedIngredients.map((ingredient, index) => (
+                <li key={index}>{ingredient.original}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
+  );
 }
