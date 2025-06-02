a30d474c00d3e0989ebfc6a0c72f68dc6c944a4b
nazarli-shabnam <shabnamnezerli@gmail.com>
2025-06-02 23:20:08 +0400

    Ingredients css

--- recipe-app/src/components/FoodDetails.jsx
+++ recipe-app/src/components/FoodDetails.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import styles from "./fooddetails.module.css"
 export default function FoodDetails({ foodId }) {
     const [isLoading, setIsLoading] = useState(true);
   const [food, setFood] = useState({});
@@ -17,37 +18,38 @@ export default function FoodDetails({ foodId }) {
   }, [foodId]);
    return (
     <div>
-      <div>
-        <h1>{food.title}</h1>
+      <div className={styles.recipeCard}>
+        <h1 className={styles.recipeTitle}>{food.title}</h1>
 
-        <img src={food.image} alt={food.title} />
+        <img className={styles.recipeImage} src={food.image} alt={food.title} />
 
-        <div>
+        <div className={styles.recipeInfo}>
           <span>
             <strong>⏰ {food.readyInMinutes} Minutes</strong>
           </span>
           <span>
             {food.servings} <strong>👪 Servings</strong>
           </span>
-          <span>
-            {food.vegetarian ? "🍲 Vegetarian" : "🍖 Not Vegetarian"}
+          <span><strong>
+            {food.vegetarian ? "🍲 Vegetarian" : "🍖 Not Vegetarian"}</strong>
           </span>
           <span>
+            <strong>
             {food.vegan ? "🌱 Vegan" : "🍖 Not Vegan"}
-          </span>
+         </strong> </span>
         </div>
 
         <div>
-          💲<span>{(food.pricePerServing / 100).toFixed(2)} Per serving</span>
+          💲<span><strong>{(food.pricePerServing / 100).toFixed(2)} Per serving</strong></span>
         </div>
       </div>
 
+        <h2>Ingredients</h2>
       <div>
         {isLoading ? (
           <p>Loading...</p>
         ) : (
-          <div>
-            <h2>Ingredients</h2>
+          <div className="ingredients">
             <ul>
               {food.extendedIngredients.map((ingredient, index) => (
                 <li key={index}>{ingredient.original}</li>
