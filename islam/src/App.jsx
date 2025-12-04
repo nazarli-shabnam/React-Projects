0fcb31dcad6ca6d9441e2c6d0a37416cbfa810ab
nazarli-shabnam <shabnamnezerli@gmail.com>
2025-12-04 11:25:05 +0400

    app.jsx

--- islam/src/App.jsx
+++ islam/src/App.jsx
@@ -8,32 +8,64 @@ import HadithCollectionExplorer from "./components/Hadith-Collection-Explorer";
 import IslamiBilgiPlatformu from "./components/İslami-Bilgi-Platformu";
 import AccountRegistrationPage from "./components/Account-Registration-Page";
 import ScrollToTop from "./components/ScrollToTop";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   return (
-    <ThemeContextProvider>
-      <CssBaseline />
-      <Router>
-        <div className="app">
-          <Button
-            component={Link}
-            to="/register"
-            variant="contained"
-            color="primary"
-            sx={{ position: "fixed", top: 16, right: 16, zIndex: 1000 }}
-          >
-            Create Free Account
-          </Button>
-          <Routes>
-            <Route path="/" element={<IslamiBilgiPlatformu />} />
-            <Route path="/reflections" element={<MyReflectionsPage />} />
-            <Route path="/hadith" element={<HadithCollectionExplorer />} />
-            <Route path="/register" element={<AccountRegistrationPage />} />
-          </Routes>
-          <ScrollToTop />
-        </div>
-      </Router>
-    </ThemeContextProvider>
+    <ErrorBoundary>
+      <ThemeContextProvider>
+        <CssBaseline />
+        <Router>
+          <div className="app">
+            <Button
+              component={Link}
+              to="/register"
+              variant="contained"
+              color="primary"
+              sx={{ position: "fixed", top: 16, right: 16, zIndex: 1000 }}
+              aria-label="Create free account"
+            >
+              Create Free Account
+            </Button>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <ErrorBoundary>
+                    <IslamiBilgiPlatformu />
+                  </ErrorBoundary>
+                }
+              />
+              <Route
+                path="/reflections"
+                element={
+                  <ErrorBoundary>
+                    <MyReflectionsPage />
+                  </ErrorBoundary>
+                }
+              />
+              <Route
+                path="/hadith"
+                element={
+                  <ErrorBoundary>
+                    <HadithCollectionExplorer />
+                  </ErrorBoundary>
+                }
+              />
+              <Route
+                path="/register"
+                element={
+                  <ErrorBoundary>
+                    <AccountRegistrationPage />
+                  </ErrorBoundary>
+                }
+              />
+            </Routes>
+            <ScrollToTop />
+          </div>
+        </Router>
+      </ThemeContextProvider>
+    </ErrorBoundary>
   );
 }
 
