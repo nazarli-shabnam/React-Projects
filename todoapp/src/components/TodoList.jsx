d20b18321c42fbae58a7a54f6376432bdce01897
nazarli-shabnam <shabnamnezerli@gmail.com>
2025-12-22 23:52:06 +0400

    bugfix

--- todoapp/src/components/TodoList.jsx
+++ todoapp/src/components/TodoList.jsx
@@ -3,7 +3,7 @@ import styles from "./todolist.module.css";
 export default function TodoList({ todos, setTodos }) {
   const sortedTodos = todos
     .slice()
-    .sort((a, b) => Number(a.done) - MutationObservermber(b.done));
+    .sort((a, b) => Number(a.done) - Number(b.done));
   return (
     <div className={styles.list}>
       {sortedTodos.map((item) => (
