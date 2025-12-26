59b736e8afe5a713959e23bae255f4b417cdbf99
nazarli-shabnam <shabnamnezerli@gmail.com>
2025-12-27 00:34:27 +0400

    add theme toggle component to App layout

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -1,14 +1,16 @@
-import Todo from './components/Todo.jsx';
-import Header from './components/Header.jsx';
-import './App.css' ;
-function App() {
+import Todo from "./components/Todo.jsx";
+import Header from "./components/Header.jsx";
+import ThemeToggle from "./components/ThemeToggle.jsx";
+import "./App.css";
 
+function App() {
   return (
     <div className="App">
-   <Header/>
-     <Todo/>
+      <Header />
+      <ThemeToggle />
+      <Todo />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
